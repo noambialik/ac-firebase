fc133f6099bb1a1aa04f4744586b97265698e311
noambialik <37776609+noambialik@users.noreply.github.com>
2018-09-19 01:16:30 +0300

    Works after changed db

--- index.js
+++ index.js
@@ -9,15 +9,6 @@ const express = require('express');
 const app = express();
 app.use(express.static('../public'));
 
-
-// app.get('/', (request,response) => {
-//     response.status(200).sendFile('/index.html');
-// });
-
-app.get('/hello', (request,response) => {
-    response.status(200).send('hello!')
-});
-
 app.post('/submitForm', (request,response) => {
     try {
         admin.initializeApp(functions.config().firebase);
@@ -30,38 +21,45 @@ app.post('/submitForm', (request,response) => {
     firestore.settings(settings);
     const db = admin.firestore();
 
-    //test
-    // const googleStorage = require('@google-cloud/storage')();
-    // const storage = googleStorage.bucket('gs://home_ac_data');
+
     const tempFilePath = path.join(os.tmpdir(), "output.csv");
     let start = request.body.start;
     let end = request.body.end;
+    start = new Date(start).valueOf();
+    end = new Date(end).valueOf();
     let j = {};
     let csv = ""
-
-    db.collection('AcStats').get().then( (q) => {
+    let pods = [];
+    db.collection('test').where("epoch",">=",start).where("epoch","<=",end).get().then( (q) => {
+    //db.collection('test').where("location","==","Home").get().then( (q) => {
         q.forEach(doc => {
             // json[doc.id.toDate()] = doc.data;
-            csv += doc.id + ',' +doc.data + '\n';
+            let data = doc.data();
+            if (!( isIn(data.location,pods) )) {
+                csv = data.location + ',' + csv;
+                pods.push(data.location);
+            }
+            csv += '\n' + data.time + ',' +data.temp + ',';
         });
 
         fs.outputFile(tempFilePath,csv)
         .then( () => {
             console.log(os.tmpdir());
-            //response.status(200).download(tempFilePath);
-            response.status(200).send(csv);
-            return 0;
+            response.status(200).download(tempFilePath);
+            // response.status(200).send(csv);
         }).catch(e => {
             throw e;
         });
-        return 0;
     }).catch(e => {
         throw e;
     });
 });
 
-app.get('/submitForm', (request,response) => {
-    response.status(200).send("found ya");
-});
+function isIn(pod,pods) {
+    return pods.some(function(el) {
+      return el === pod;
+    }); 
+  }
+
 exports.main = functions.https.onRequest(app);
 
